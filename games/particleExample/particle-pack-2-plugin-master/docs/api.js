6eaa0a8dd269691359349f366b54dcba2e393759
zacharyf1991 <zachary.freiberg@gmail.com>
2014-09-08 14:54:38 +1200

    upload particle example

---
+++ games/particleExample/particle-pack-2-plugin-master/docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Kiwi.Plugins.ParticlePack1"
+    ],
+    "modules": [
+        "Kiwi",
+        "Kiwi.Plugins"
+    ],
+    "allModules": [
+        {
+            "displayName": "Kiwi",
+            "name": "Kiwi"
+        },
+        {
+            "displayName": "Kiwi.Plugins",
+            "name": "Kiwi.Plugins",
+            "description": "For a walkthrough on setting up the particle pack plugin check the <a href=\"https://github.com/gamelab/particle-pack-1-plugin/blob/master/README.md\"><README./a>"
+        }
+    ]
+} };
+});
